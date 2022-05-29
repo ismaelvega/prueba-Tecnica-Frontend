a3fdac2fbf65eab846d13cf0a3e67e7b5dfb8756
Ismael Vega <ismaelgrive@gmail.com>
2022-05-29 07:07:45 -0500

    Ejercicio resuelto

--- resolucion-ejercicio/no-ts/main.js
+++ resolucion-ejercicio/no-ts/main.js
@@ -5,6 +5,25 @@ exports.solucion = void 0;
 //resultado esperado testCase2: [2063524951, 2802298787]
 //resultado esperado testCase3: [1659655705 , 2484892405]
 function solucion(numeros) {
-    return [];
+    const sortedArray = numeros.sort((a, b) => {
+        if(a<b) {
+          return -1
+        }
+        if(a>b){
+          return 1
+        }
+        return 0
+      })
+  
+      const min = sortedArray.slice()
+      min.shift()
+      const minSum = min.reduce((n, sum) => sum += n)
+  
+      const max = sortedArray.slice()
+      max.pop()
+      const maxSum = max.reduce((n, sum) => sum += n)
+      console.log(maxSum, minSum );
+
+      return [maxSum, minSum]
 }
 exports.solucion = solucion;
